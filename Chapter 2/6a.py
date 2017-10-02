d6bcd33f24bb56a23e90ca443a1379ad6285372f
Liam <31958679+liam-pk@users.noreply.github.com>
2017-10-02 08:58:21 -0400

    the rest of chapter 3

---
+++ Chapter 2/6a.py
@@ -0,0 +1,11 @@
+import turtle
+def draw_triangle():
+    window = turtle.Screen()
+    tom = turtle.Turtle()
+    tom.forward(100) 
+    tom.left(120)
+    tom.forward(100)
+    tom.left(120)
+    tom.forward(100)
+    window.exitonclick()  
+draw_triangle()
---
+++ Chapter 2/6d.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.pendown()
+for i in range(8):
+    turtle.forward(100)
+    turtle.right(360/8)
+
+turtle.forward(50)
+for i in range(8):
+       turtle.forward
+       turtle.right(360/8)
